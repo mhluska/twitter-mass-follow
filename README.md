fd1ceb6db72a5a4f32d42980791f64aa3134b551
Maros Hluska <mhluska@gmail.com>
2015-01-04 16:16:06 -0500

    Add Gemfile, script, readme, sample follow.txt file

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ twitter-mass-follow
 ===================
 
 Follow a list of Twitter profile URLs
+
+I used this to follow 150+ Bitcoin-related Twitter users from http://blog.coingecko.com/top-cryptocurrency-people-to-follow/.
+
+1. I first retrieve the list using this JavaScript in the console:
+```
+	document.querySelectorAll('a[href^="https://twitter.com/"]').map(function (item){ return item.href; }).join('\n');
+```
+
+2. Copy-paste into the file `follow.txt`
+
+3. Make an app on https://apps.twitter.com/. Add the relevant keys to `.env` like so:
+
+```
+TWITTER_CONSUMER_KEY=<key>
+TWITTER_CONSUMER_SECRET=<secret>
+TWITTER_ACCESS_TOKEN=<token>
+TWITTER_ACCESS_SECRET=<secret>
+```
+
+4. Run the script with `bundle exec ruby follow.rb`. Or run it in the background
+   with `bundle exec ruby follow.rb --silent &`.
---
+++ follow.rb
@@ -0,0 +1,41 @@
+require 'twitter'
+require 'dotenv'
+
+Dotenv.load
+
+SLEEP_TIME = 30
+silent     = ARGV.include?('--silent') || ARGV.include?('-s')
+
+# See http://stackoverflow.com/a/10263337
+def show_wait_cursor(seconds, fps=10)
+  chars = %w[| / - \\]
+  delay = 1.0/fps
+  (seconds * fps).round.times{ |i|
+    print chars[i % chars.length]
+    sleep delay
+    print "\b"
+  }
+end
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+end
+
+File.readlines('follow.txt').each do |line|
+  username = line.split('/').pop
+  client.follow(username)
+
+  puts "Followed @#{username}" unless silent
+
+  # Avoid rate limit issues with Twitter API.
+  if silent
+    sleep SLEEP_TIME
+  else
+    print "Pausing for #{SLEEP_TIME} seconds "
+    show_wait_cursor(SLEEP_TIME)
+    print "\n"
+  end
+end
